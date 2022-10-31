914237036f18b6ed0602b3e0f0cefbf38c818611
Eduardo968 <73999000+Eduardo968@users.noreply.github.com>
2022-10-31 17:54:52 -0300

    Add files via upload

---
+++ tupinigames.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 31-Out-2022 às 20:54
+-- Versão do servidor: 5.7.36
+-- versão do PHP: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `tupinigames`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `jogos`
+--
+
+DROP TABLE IF EXISTS `jogos`;
+CREATE TABLE IF NOT EXISTS `jogos` (
+  `id_jogo` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_jogo` varchar(500) DEFAULT NULL,
+  `data_lancamento` date DEFAULT NULL,
+  `valor` double DEFAULT NULL,
+  PRIMARY KEY (`id_jogo`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_user` varchar(45) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `CPF` varchar(16) DEFAULT NULL,
+  `cep` varchar(20) DEFAULT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `senha` varchar(500) DEFAULT NULL,
+  `rua` varchar(750) DEFAULT NULL,
+  `bairro` varchar(100) DEFAULT NULL,
+  `estado` varchar(30) DEFAULT NULL,
+  `cidade` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `CPF` (`CPF`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
